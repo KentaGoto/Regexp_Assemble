96f2f77ad954fb39719dad7f9b1f7b5868f9f5ad
KentaGoto <gotokentag@gmail.com>
2019-12-02 11:21:55 +0900

    1st

---
+++ Regexp_Assemble.pl
@@ -0,0 +1,45 @@
+use strict;
+use warnings;
+use utf8;
+use Encode;
+use Regexp::Assemble::Compressed;
+use Clipboard;
+use Win32::Clipboard;
+use Win32;
+
+binmode STDIN,  ':encoding(cp932)';
+binmode STDOUT, ':encoding(cp932)';
+binmode STDERR, ':encoding(cp932)';
+
+##############################################################
+### [概要]
+### 正規表現を生成する。
+### 生成された正規表現はクリップボードにコピーされる。
+##############################################################
+
+# クリップボードから対象文字列を得る
+my $text_tmp = Win32::Clipboard();
+my $text_tmp_decode_paste = $text_tmp->GetAs(CF_UNICODETEXT);
+my $text  = decode('UTF16-LE', $text_tmp_decode_paste);
+my @text_list = split(/\n/, $text);
+
+my $builder = Regexp::Assemble::Compressed->new;
+
+for ( @text_list ) {
+    chomp;
+    $builder->add( quotemeta $_ );
+}
+
+my $regex = $builder->re;
+
+# 各種エディターでも使えるように正規表現を整形（Perlで使うならいらない）
+$regex =~ s{^\(\?\^u?:}{};
+$regex =~ s{\)$}{};
+
+my $output = 'output.txt';
+open my $out, '>:encoding(utf8)', $output or die $!;
+print {$out} $regex;
+close $out;
+
+system ("notepad $output");
+
---
+++ Regexp_Assemble_GUI.ahk
@@ -0,0 +1,30 @@
+﻿#NoEnv  ; Recommended for performance and compatibility with future AutoHotkey releases.
+; #Warn  ; Enable warnings to assist with detecting common errors.
+SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
+SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
+
+Gui, Add, Edit, Vinputtext x12 y10 w450 h320 , ここに改行区切りのテキストを入力してください
+Gui, Add, Button, gExec x362 y340 w100 h30 , OK
+; Generated using SmartGUI Creator 4.0
+Gui, Show, x127 y87 h379 w479, テキスト入力ウィンドウ
+Return
+
+;;; Main
+Exec:
+
+; 入力されたテキストを変数に代入
+Gui, Submit, NoHide
+
+; 変数の値をクリップボードにコピー
+Clipboard = %inputtext%
+ClipWait,1
+;MsgBox, %Clipboard%
+Sleep, 30
+
+cmd = wperl Regexp_Assemble.pl
+Run , %cmd%
+
+Return
+
+GuiClose:
+ExitApp
